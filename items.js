ae25ff6d6b3eeb0f4bb11be4db8ee0ec0a014340
H0tKinsS <h0tkinss1@gmail.com>
2024-02-20 21:28:12 +0100

    Added new backgrounds

--- items.js
+++ items.js
@@ -21,5 +21,19 @@ const items = [
 		description: 'Wydobyte z wnętrzności zniszczonych piniat, mogą służyć do wymian między graczami',
 		category: 'garden_background'
 	},
+	{
+		name: 'Kamienne tło ogrodu',
+		id: 'stone garden background',
+		emoji: ':park:',
+		description: 'Wydobyte z wnętrzności zniszczonych piniat, mogą służyć do wymian między graczami',
+		category: 'garden_background'
+	},
+	{
+		name: 'Magmowe tło ogrodu',
+		id: 'magma garden background',
+		emoji: ':park:',
+		description: 'Wydobyte z wnętrzności zniszczonych piniat, mogą służyć do wymian między graczami',
+		category: 'garden_background'
+	},
 ];
 module.exports = items;
